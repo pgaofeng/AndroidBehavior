313e89df4b87e1bb843828486575f0ea648ccc14
gaofengpeng <gaofengpeng1@163.com>
2021-09-30 09:56:49 +0800

    新建项目

---
+++ app/src/main/java/com/study/androidbehavior/widget/MovableButton.kt
@@ -0,0 +1,40 @@
+package com.study.androidbehavior.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatButton
+
+/**
+ * @author gaofeng
+ * @date 2021/9/30
+ *
+ * 可移动的Button，可以跟随手指的滑动而变化位置。
+ */
+class MovableButton @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null
+) : AppCompatButton(context, attributeSet) {
+
+    private var mInitX = 0F
+    private var mInitY = 0F
+    private var mEventX = 0F
+    private var mEventY = 0F
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                mInitX = x
+                mInitY = y
+                mEventX = event.rawX
+                mEventY = event.rawY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                x = mInitX + event.rawX - mEventX
+                y = mInitY + event.rawY - mEventY
+            }
+        }
+        return true
+    }
+
+}
