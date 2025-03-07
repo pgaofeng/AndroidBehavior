f27cc003c3df2eec085e6d1f0a255f1f597b3b68
pgaofeng <gaofengpeng1@163.com>
2025-03-07 12:31:04 +0800

    优化多指滑动

--- app/src/main/java/com/study/androidbehavior/widget/MyViewGroup.kt
+++ app/src/main/java/com/study/androidbehavior/widget/MyViewGroup.kt
@@ -3,6 +3,7 @@ package com.study.androidbehavior.widget
 import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
+import android.util.SparseArray
 import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.animation.DecelerateInterpolator
@@ -19,23 +20,29 @@ class MyViewGroup @JvmOverloads constructor(
     private val max = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300F, context.resources.displayMetrics)
 
     // 手指按下的坐标
-    private var downY  = 0F
+    private val downY  = SparseArray<Float>()
     // 应当滚动的距离
     private var scrollTop = 0F
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when(event?.actionMasked) {
             // 按下时记录坐标
-            MotionEvent.ACTION_DOWN -> downY = event.y
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN -> {
+                downY[event.getPointerId(event.actionIndex)] = event.getY(event.actionIndex)
+            }
             MotionEvent.ACTION_MOVE -> {
                 // 当前滚动的距离
-                val deltaY = event.y - downY
-                // 计算deltaY对应的实际滚动距离
-                scrollTop = scrollTop + compute(scrollTop + deltaY) - compute(scrollTop)
-                // 滚动到对应的位置
-                scrollTo(scrollX, -scrollTop.toInt())
-                // 本次滑动结束，更新坐标
-                downY = event.y
+                println("move == ${event.pointerCount}")
+                for (i in 0 until event.pointerCount) {
+                    val id = event.getPointerId(i)
+                    val deltaY = event.getY(i) - downY[id]
+                    // 计算deltaY对应的实际滚动距离
+                    scrollTop = scrollTop + compute(scrollTop + deltaY) - compute(scrollTop)
+                    // 滚动到对应的位置
+                    scrollTo(scrollX, -scrollTop.toInt())
+                    // 本次滑动结束，更新坐标
+                    downY[id] = event.getY(i)
+                }
             }
             MotionEvent.ACTION_UP -> {
                 // 手指抬起时恢复到初始位置
