a17024d55c2de154c7554b5fc01843a10ade17fe
pgaofeng <gaofengpeng1@163.com>
2025-03-07 13:54:26 +0800

    支持向上滑动

--- app/src/main/java/com/study/androidbehavior/widget/MyViewGroup.kt
+++ app/src/main/java/com/study/androidbehavior/widget/MyViewGroup.kt
@@ -7,14 +7,15 @@ import android.util.SparseArray
 import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.animation.DecelerateInterpolator
-import android.widget.LinearLayout
+import android.widget.FrameLayout
+import kotlin.math.abs
 import kotlin.math.pow
 
 class MyViewGroup @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     def: Int = 0
-) : LinearLayout(context, attributeSet, def){
+) : FrameLayout(context, attributeSet, def) {
 
     // 300dp，函数中的常量N
     private val max = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300F, context.resources.displayMetrics)
@@ -32,7 +33,6 @@ class MyViewGroup @JvmOverloads constructor(
             }
             MotionEvent.ACTION_MOVE -> {
                 // 当前滚动的距离
-                println("move == ${event.pointerCount}")
                 for (i in 0 until event.pointerCount) {
                     val id = event.getPointerId(i)
                     val deltaY = event.getY(i) - downY[id]
@@ -44,11 +44,12 @@ class MyViewGroup @JvmOverloads constructor(
                     downY[id] = event.getY(i)
                 }
             }
+
             MotionEvent.ACTION_UP -> {
                 // 手指抬起时恢复到初始位置
                 ValueAnimator.ofFloat(scrollTop, 0F).also {
                     it.interpolator = DecelerateInterpolator()
-                    it.addUpdateListener { anim->
+                    it.addUpdateListener { anim ->
                         scrollTop = anim.animatedValue as Float
                         scrollTo(scrollX, -scrollTop.toInt())
                     }
@@ -61,7 +62,11 @@ class MyViewGroup @JvmOverloads constructor(
 
     // 映射函数：f(x) = N * (1 - 2^(-x/N))
     private fun compute(x: Float): Float {
-        return max * (1 - 2F.pow(-x/max))
+        var signal = 1
+        if (x < 0) {
+            signal = -1
+        }
+        return max * (1 - 2F.pow(-abs(x) / max)) * signal
     }
 
 }
